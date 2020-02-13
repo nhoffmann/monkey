3f070dc54fb48f4cb7b73ac4251f8637b915541c
Niels Hoffmann <niels@zentralmaschine.net>
2020-02-13 13:25:40 +0100

    Create a REPL that outputs the lexed line

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/nhoffmann/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the monkey programming language.\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/nhoffmann/monkey/lexer"
+	"github.com/nhoffmann/monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lexer := lexer.NewLexer(line)
+
+		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
