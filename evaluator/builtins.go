1d18a45d00a17fdf31ca7d83ce809044145138e4
Niels Hoffmann <niels@zentralmaschine.net>
2020-03-03 12:56:07 +0100

    Add some array functions

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -23,6 +23,82 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"first": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument, expected 1, got %d", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY {
+				return newError("argument must be ARRAY, got %s", args[0].Type())
+			}
+
+			array := args[0].(*object.Array)
+			if len(array.Elements) > 0 {
+				return array.Elements[0]
+			}
+			return NULL
+		},
+	},
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument, expected 1, got %d", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY {
+				return newError("argument must be ARRAY, got %s", args[0].Type())
+			}
+
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+			if length > 0 {
+				return array.Elements[length-1]
+			}
+			return NULL
+		},
+	},
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument, expected 1, got %d", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY {
+				return newError("argument must be ARRAY, got %s", args[0].Type())
+			}
+
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+			if length > 0 {
+				newElements := make([]object.Object, length-1, length-1)
+				copy(newElements, array.Elements[1:length])
+
+				return &object.Array{Elements: newElements}
+			}
+			return NULL
+		},
+	},
+	"push": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of argument, expected 2, got %d", len(args))
+			}
+
+			if args[0].Type() != object.ARRAY {
+				return newError("argument must be ARRAY, got %s", args[0].Type())
+			}
+
+			array := args[0].(*object.Array)
+			length := len(array.Elements)
+
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, array.Elements)
+			newElements[length] = args[1]
+
+			return &object.Array{Elements: newElements}
+		},
+	},
 	"puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			for _, arg := range args {
