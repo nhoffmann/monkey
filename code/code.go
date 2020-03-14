350db7a1eade66c10e8d25e9521163063427a5fe
Niels Hoffmann <niels@zentralmaschine.net>
2020-03-14 23:32:52 +0100

    Compile and execute index operations

--- code/code.go
+++ code/code.go
@@ -52,6 +52,7 @@ const (
 	OpGetGlobal
 	OpArray
 	OpHash
+	OpIndex
 )
 
 type Definition struct {
@@ -80,6 +81,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
 	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
